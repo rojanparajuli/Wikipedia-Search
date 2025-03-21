3162f1fc65b12ce1aaea77a217be9a5172f57cac
rojanparajuli <rojanparajuli13@gmail.com>
2025-03-21 20:55:54 +0545

    corrcetion

---
+++ lib/bloc/splash/splash_screen_state.dart
@@ -0,0 +1,21 @@
+
+abstract class SplashState  {
+  List<Object?> get props => [];
+}
+
+class SplashInitial extends SplashState {}
+
+class CheckingInternetState extends SplashState {}
+
+class InternetAvailableState extends SplashState {
+  final int progress;
+
+  InternetAvailableState(this.progress);
+
+  @override
+  List<Object?> get props => [progress];
+}
+
+class InternetNotAvailableState extends SplashState {}
+
+class SplashCompletedState extends SplashState {}
--- lib/view/wiki_screen.dart
+++ lib/view/wiki_screen.dart
@@ -1,5 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:wiki/bloc/wiki_bloc.dart';
 import 'package:wiki/bloc/wiki_event.dart';
 import 'package:wiki/bloc/wiki_state.dart';
@@ -13,35 +16,36 @@ class WikipediaSearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Wikipedia Search', style: TextStyle(fontWeight: FontWeight.bold , color: Colors.white)),
+        title: Text(
+          'Wikipedia Search',
+          style: GoogleFonts.lato(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.white,
+        elevation: 2,
+        iconTheme: const IconThemeData(color: Colors.black),
       ),
       body: Container(
         padding: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.deepPurple.shade700, Colors.deepPurple.shade300],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
+        color: Colors.grey.shade100,
         child: Column(
           children: [
             Material(
-              elevation: 5,
+              elevation: 3,
               borderRadius: BorderRadius.circular(30),
               child: TextField(
                 controller: _controller,
                 decoration: InputDecoration(
                   hintText: 'Search Wikipedia...',
-                  prefixIcon: Icon(Icons.search, color: Colors.deepPurple),
+                  hintStyle: GoogleFonts.lato(),
+                  prefixIcon: const Icon(Icons.search, color: Colors.blue),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(30),
                     borderSide: BorderSide.none,
                   ),
                   filled: true,
                   fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.symmetric(vertical: 14),
                 ),
                 onSubmitted: (query) {
                   context.read<WikipediaBloc>().add(SearchWikipedia(query));
@@ -53,7 +57,23 @@ class WikipediaSearchScreen extends StatelessWidget {
               child: BlocBuilder<WikipediaBloc, WikipediaState>(
                 builder: (context, state) {
                   if (state is WikipediaLoading) {
-                    return Center(child: CircularProgressIndicator(color: Colors.white));
+                    return ListView.builder(
+                      itemCount: 5,
+                      itemBuilder: (context, index) {
+                        return Shimmer.fromColors(
+                          baseColor: Colors.grey.shade300,
+                          highlightColor: Colors.grey.shade100,
+                          child: Container(
+                            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                            height: 80,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                        );
+                      },
+                    );
                   } else if (state is WikipediaLoaded) {
                     return ListView.builder(
                       itemCount: state.results.length,
@@ -63,31 +83,41 @@ class WikipediaSearchScreen extends StatelessWidget {
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
-                          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                           child: ListTile(
-                            title: Text(result['title'],
-                                style: TextStyle(fontWeight: FontWeight.bold)),
+                            title: Text(
+                              result['title'],
+                              style: GoogleFonts.lato(fontWeight: FontWeight.bold, color: Colors.black),
+                            ),
                             subtitle: Text(
                               result['snippet'].replaceAll(RegExp(r'<[^>]*>'), ''),
                               maxLines: 2,
                               overflow: TextOverflow.ellipsis,
+                              style: GoogleFonts.lato(color: Colors.grey.shade700),
                             ),
-                            trailing: Icon(Icons.arrow_forward_ios, color: Colors.deepPurple),
-                            onTap: () {},
+                            trailing: const Icon(Icons.arrow_forward_ios, color: Colors.blue),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => WikipediaDetailScreen(title: result['title']),
+                                ),
+                              );
+                            },
                           ),
                         );
                       },
                     );
                   } else if (state is WikipediaError) {
-                    return Center(
+                    return const Center(
                       child: Text('Error fetching data',
-                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                          style: TextStyle(color: Colors.red, fontSize: 16)),
                     );
                   }
                   return Center(
                     child: Text(
                       'Search for Wikipedia articles',
-                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      style: GoogleFonts.lato(color: Colors.black, fontSize: 18),
                     ),
                   );
                 },
@@ -95,6 +125,55 @@ class WikipediaSearchScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class WikipediaDetailScreen extends StatelessWidget {
+  final String title;
+
+  const WikipediaDetailScreen({super.key, required this.title});
+
+  Future<String> fetchWikipediaImageUrl(String title) async {
+    final response = await Dio().get(
+      'https://en.wikipedia.org/w/api.php',
+      queryParameters: {
+        'action': 'query',
+        'format': 'json',
+        'prop': 'pageimages',
+        'piprop': 'original',
+        'titles': title,
+      },
+    );
+    var pages = response.data['query']['pages'];
+    var page = pages.values.first;
+    return page.containsKey('original') ? page['original']['source'] : '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: FutureBuilder<String>(
+        future: fetchWikipediaImageUrl(title),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No image available'));
+          }
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.network(snapshot.data!),
+                const SizedBox(height: 20),
+                Text(title, style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
